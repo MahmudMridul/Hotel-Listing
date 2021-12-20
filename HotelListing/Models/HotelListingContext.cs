20990bc7a3bfa451c32d17d0dd1e518b1f57ec93
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-12-20 09:28:34 +0600

    Model classes added | DB conn string added

---
+++ HotelListing/Models/HotelListingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class HotelListingContext : DbContext
+    {
+        public HotelListingContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
