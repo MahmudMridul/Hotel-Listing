44f08a8af591628ae2f9facbfb8e2dc99c4520c9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-12-21 16:00:39 +0600

    DTO Addition

---
+++ HotelListing/DTOs/CountryDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.DTOs
+{
+    public class CreateCountryDTO
+    {
+
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Name too long")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Code { get; set; }
+    }
+
+    public class CountryDTO : CreateCountryDTO 
+    {
+        public int Id { get; set; }
+    }
+
+}
---
+++ HotelListing/DTOs/HotelDTO.cs
@@ -0,0 +1,34 @@
+﻿using HotelListing.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.DTOs
+{
+    public class CreateHotelDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        public double Rating { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+    }
+
+    public class HotelDTO : CreateHotelDTO
+    {
+        public int Id { get; set; }
+        
+        public CountryDTO Country { get; set; }
+    }
+
+}
