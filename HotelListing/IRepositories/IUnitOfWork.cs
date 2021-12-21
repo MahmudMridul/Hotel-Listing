516405b9a6f279f24451000e0f5f54c935db14e1
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2021-12-21 08:47:32 +0600

    IUnitOfWork Interface Addition

---
+++ HotelListing/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using HotelListing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.IRepositories
+{
+    /* The primary use of this interface(IDisposable) is to release 
+     * unmanaged resources. The garbage collector automatically 
+     * releases the memory allocated to a managed object 
+     * when that object is no longer used. However, it is not 
+     * possible to predict when garbage collection will occur. */
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
